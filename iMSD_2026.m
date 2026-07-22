d3217f91a58cf5e6272e3f4066873621bc5036e2
lucadigiacom0 <luca.digiacomo@uniroma1.it>
2026-07-22 10:42:04 +0200

    Update iMSD_2026.m

--- iMSD_2026.m
+++ iMSD_2026.m
@@ -1,33 +1,33 @@
 clear all
-clear all
-clc
 close all
+clc
 
-filename = 'test_imagestack.tif';
+tic
+
+filename='test_imagestack.tif';
+export_value = 0;
+
 disp('     importing image-stack')
-
-export_value=1;
-
 imsd_01_import
-A=double(imgStack);
+A = double(imgStack);
 
-%%% input parameters
-px_size= 0.04824;                  %%% pixel size (micron)
-frame_time=0.192;                  %%% time between two successive frames (s)
+%%% input parameters (edit for your acquisition)
+px_size    = 0.05;                 %%% pixel size (micron)
+frame_time = 0.192;                %%% time between successive frames (s)
+PSF_waist  = 0.25;                 %%% PSF waist from calibration
 
-%%%%
-N=size(A,3);                       %%% number of frames
-P=size(A,1);                       %%% lateral size (px)
-R=px_size*P;                       %%% lateral size (micron)
+N = size(A,3);
+P = size(A,1);
+R = px_size*P;
 
-%%% spatial-temporal domain
-SpatialLimit=32;                   %%% linear extent of spatial domain (px)
-                                   %%% only even values
-TauLimit=int64(N/5)+1;          %%% lag-time domain (default N/10)
+SpatialLimit = 24;                 %%% linear extent of spatial domain (px, even)
+TauLimit     = int64(N/10)+1;      %%% lag-time domain
+fitt2comp = false;                 %%% true = two-component fit (two iMSD curves)
 
-time_vec=frame_time*(1:double(TauLimit))';
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+time_vec     = frame_time*(1:double(TauLimit))';
 
-imsd_02_spatial 
+imsd_02_spatial
 imsd_03_spatiotemporal
 imsd_04_output
+
+toc
